fc33cf135a73c3f2943b4e106c2f402789ffc0a9
Becario <becario.desarrollo@estrellaroja.com.mx>
2022-12-15 11:10:38 -0600

    js

---
+++ Actividad5.js
@@ -0,0 +1,25 @@
+//string
+let nombre = "Maria Fernanda ";
+let megusta = "Los gatos"
+
+//Number
+let edad = 23;
+let x = 5;
+let y = 12;
+
+//Bolean
+let verdadero = true;
+let falso = false;
+
+//undefined 
+let indefinido = undefined;
+
+
+//interpolación de texto:
+console.log(`Hola mundo, mi nombre es ${nombre} y tengo ${edad} años, me gustan ${megusta}`);
+
+console.log(x*y);
+
+console.log(y += x);
+
+console.log(x != y);
