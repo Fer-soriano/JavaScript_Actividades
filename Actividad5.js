02b0118e013fcf7bcc23898c83657b1224c6755f
Becario <becario.desarrollo@estrellaroja.com.mx>
2022-12-16 11:20:18 -0600

    js

--- Actividad5.js
+++ Actividad5.js
@@ -25,11 +25,13 @@ console.log(y += x);
 console.log(x != y);
 
 
-/*Ejemplo de clase */
-//prompt pedi datos al usuario */
+/*Ejemplo de clase 
+ //prompt pide datos al usuario 
 let alumnos = 1;
 
 for(alumnos; alumnos <= 10; alumnos++){
     let nombrealumno = prompt ("como te llamas?");
     console.log("El alumno se llama  " + nombrealumno + " va hacer la consulta " + alumnos )
+    alert (`El alumno se llama: ${nombrealumno} va hacer la consulta ${alumnos}`)
 }
+*/
